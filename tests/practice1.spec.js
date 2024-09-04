6d53b57b0d88629c549511752686d693092af98f
Tommy Hoang <hoangtommyquoc@gmail.com>
2024-09-03 22:39:56 -0500

    comment out the import

--- tests/practice1.spec.js
+++ tests/practice1.spec.js
@@ -1,5 +1,7 @@
 const {test, expect} = require ('@playwright/test')
 const {hello, helloworld} = require ('./demo/hello')
+// import {hello, helloworld} from './demo/hello'
 
 console.log(hello());
 console.log(helloworld());
+
