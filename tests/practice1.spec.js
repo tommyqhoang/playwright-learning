bfb2328649d6a563f3cbcaed9b53f04f40dd6d5e
Tommy Hoang <hoangtommyquoc@gmail.com>
2024-09-03 23:05:09 -0500

    Search google title for google.com

--- tests/practice1.spec.js
+++ tests/practice1.spec.js
@@ -5,3 +5,7 @@ const {hello, helloworld} = require ('./demo/hello')
 console.log(hello());
 console.log(helloworld());
 
+test('My first test', async({page})=> {
+    await page.goto('https://google.com')
+    await expect(page).toHaveTitle('Google')
+})
